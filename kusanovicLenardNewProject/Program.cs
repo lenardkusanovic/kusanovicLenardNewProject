85fff5ee96d89ca438877ed6ba6077cc911dff06
lenardkusanovic <55271599+lenardkusanovic@users.noreply.github.com>
2019-10-17 11:34:22 +0200

    Add files via upload

---
+++ kusanovicLenardNewProject/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kusanovicLenardNewProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            float y =18.5F;
+            long x = Convert.ToInt32(y);
+            double z = y;
+
+            Console.WriteLine("y = " + y);
+            Console.WriteLine("x = " + x);
+            Console.WriteLine("z = " + z);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
